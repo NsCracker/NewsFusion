52251fdc0068f85a42ee1570ee3cdeb1a9c04f4c
Ns Cracker <115659007+NsCracker@users.noreply.github.com>
2023-09-16 03:24:25 +0530

    fix badge

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,9 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" >
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:"1", left:"90%"}}>{source}</span>
+          <div>
+            <span className="badge rounded-pill bg-danger" style={{display: "flex", justifyContent: "flex-end", position: "absolute", right: "2px", top: "2px"}}>{source}</span>
+          </div>
             <img src={imageUrl?imageUrl:"https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=612x612&w=0&k=20&c=9pR2-nDBhb7cOvvZU_VdgkMmPJXrBQ4rB1AkTXxRIKM="} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
